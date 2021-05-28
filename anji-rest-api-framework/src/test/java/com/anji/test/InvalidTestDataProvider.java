eca25d09fdc17ef422104044c7e8f95eea66f4fd
Boddupally.Anji <boddupally.anji@Ss-MacBook-Pro.home>
2021-05-29 01:06:05 +0200

    Fixed missing java doc

--- anji-rest-api-framework/src/test/java/com/anji/test/InvalidTestDataProvider.java
+++ anji-rest-api-framework/src/test/java/com/anji/test/InvalidTestDataProvider.java
@@ -4,6 +4,13 @@ import org.testng.annotations.DataProvider;
 
 import com.anji.mendix.api.data.TestDataFactory;
 
+/**
+ * 
+ * Data provider class that caters to the tests for various scenarios.
+ * 
+ * @author boddupally.anji
+ */
+
 public class InvalidTestDataProvider {
 
 	@DataProvider(name = "invalid-article-data-provider")
